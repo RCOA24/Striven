8b676ab793c796bd292095d23f5bc656511be5a0
Rodney Austria <mrtangkad59@gmail.com>
2025-11-02 11:12:12 +0800

    rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Activity } from 'lucide-react';
-import useStriven from '../hooks/useStriven';
+import useStriven from './hooks/UseStriven';
 import useNotifications from './hooks/useNotifications';
 import MainLayout from './components/MainLayout';
 import Dashboard from './pages/Dashboard';
